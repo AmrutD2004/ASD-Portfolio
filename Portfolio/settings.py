e17428f19670aa828ebdc5e717b317f31d592c47
Amrut D <amrutd2004@gmail.com>
2025-07-09 16:45:27 +0530

    second commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7$j7s+g1ivd!y1$rgqdn&+wsn$$jsj&2y&$_*t)+n1%pp$k^*i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['.onrender.com']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Portfolio.urls'
